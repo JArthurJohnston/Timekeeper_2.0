9242990a9db12aa8faa1ca34c6bb9c778fb5abba
JArthurJohnston <joe.a.johnston@gmail.com>
2015-10-14 19:28:04 -0400

    Added some css to make the menu more menuy. and to make the buttons bar visible. Both the buttons bar and menu are now always on top and fixed to the bottom and top of the screen

--- app/controllers/statements_of_work_controller.rb
+++ app/controllers/statements_of_work_controller.rb
@@ -20,13 +20,16 @@ class StatementsOfWorkController < ApplicationController
 
   def update
     @statement_of_work = find_sow
-    @statement_of_work.update(sow_params)
+    @statement_of_work.update!(sow_params)
     redirect_to user_statement_of_work_path(@user.id, params[:id])
   end
 
   def create
-    @statement_of_work = StatementOfWork.create(sow_params)
-    redirect_to user_statements_of_work_path(@user.id, @statement_of_work.id)
+    @statement_of_work = StatementOfWork.new(sow_params)
+    @statement_of_work.user_id = @user.id
+    @statement_of_work.save!
+    redirect_to action: :show, id: @statement_of_work.id, user_id: @user.id
+    # redirect_to user_statements_of_work_path(@user.id, @statement_of_work.id)
   end
 
   def sow_params
