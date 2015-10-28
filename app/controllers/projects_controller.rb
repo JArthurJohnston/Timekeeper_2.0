26cc7bcc5c53dfa199bea4174594cf1529b4482a
JArthurJohnston <joe.a.johnston@gmail.com>
2015-10-28 08:07:03 -0400

    Fixed the project index action.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,9 @@
 class ProjectsController < ApplicationController
 
+  def index
+    @projects = Project.all
+  end
+
   def show
     @project = Project.find_by(user_id: @user.id, id: params[:id])
   end
