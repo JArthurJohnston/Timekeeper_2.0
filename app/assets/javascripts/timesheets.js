854a4a07aae1fa9d92c8d4849bf41f9d23d673df
JArthurJohnston <joe.a.johnston@gmail.com>
2016-02-23 15:44:24 -0500

    removed the javascript files that werent being used. and the references to them in assets.rb that were truying to precompile them.

--- app/assets/javascripts/timesheets.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by arthur on 2/22/16.
- */
-
-// This is a manifest file that'll be compiled into timesheet.js, which will include all the files
-// listed below.
-
-//= require_tree ./timesheets
